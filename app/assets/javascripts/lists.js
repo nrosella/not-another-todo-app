9e9ef8a53983ccaa0f71ecaa337b1b927db7d148
nrosella <ninorosella@gmail.com>
2015-10-07 20:59:11 +0100

    started work on log in permissions

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -8,40 +8,7 @@ $(document).on("page:load", function(){
     $(this).parents("form").trigger("submit");
   })
 
-	// $('#form-input').click(function() {
-	//   if ($(this).is(':checked')) {
-	//   	$('#item-one-complete').addClass("strikethrough")
-	//   } else {
-	//     $('#item-one-complete').removeClass("strikethrough")
-	//   }
-	// });
-
-	// $('#form-input-two').click(function() {
-	//   if ($(this).is(':checked')) {
-	//   	$('#item-two-complete').addClass("strikethrough")
-	//   } else {
-	//     $('#item-two-complete').removeClass("strikethrough")
-	//   }
-	// });
-
-	// $('#form-input-three').click(function() {
-	//   if ($(this).is(':checked')) {
-	//   	$('#item-three-complete').addClass("strikethrough")
-	//   } else {
-	//     $('#item-three-complete').removeClass("strikethrough")
-	//   }
-	// });
-
-	// $('#form-input-four').click(function() {
-	//   if ($(this).is(':checked')) {
-	//   	$('#item-four-complete').addClass("strikethrough")
-	//   } else {
-	//     $('#item-four-complete').removeClass("strikethrough")
-	//   }
-	// });
-
 	$('#completed-button').click(function(){
-		console.log("Marked all as completed");
 		$('#item-one-complete').addClass("strikethrough");
 		$('#item-two-complete').addClass("strikethrough");
 		$('#item-three-complete').addClass("strikethrough");
