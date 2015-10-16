1818c697729d5e9f6638e19d271e273e9bb5d5b6
nrosella <ninorosella@gmail.com>
2015-10-16 18:09:53 +0100

    tweaked js error message for jquery

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -13,7 +13,7 @@ $(function(){
 
 })
 
-document.addEventListener("DOMContentLoaded", function() {
+$(document).addEventListener("DOMContentLoaded", function() {
     var elements = document.getElementsByTagName("INPUT");
     for (var i = 0; i < elements.length; i++) {
         elements[i].oninvalid = function(e) {
