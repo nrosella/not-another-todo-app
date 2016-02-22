433bf6820c11b7b5591642291f7614a820fefedd
nrosella <ninorosella@gmail.com>
2016-02-22 13:12:37 +0000

    remove commented out code to tidy up

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,9 +1,6 @@
 class ListsController < ApplicationController
 
 	def index
-		# binding.pry
-		# if current_user != 
-		# @user = List.find(params[:id]).user_id
 		@list = List.new
 		@lists = List.order("id")
 	end
@@ -13,11 +10,6 @@ class ListsController < ApplicationController
 		@list.user_id = current_user.id
 	  if @list.save
 	  	redirect_to list_path(@list)
-      # if request.xhr?
-      # 	render :layout => false
-      # else
-      # 	redirect_to root_path
-      # end
     else
       @lists = List.all
       render :index
@@ -36,7 +28,6 @@ class ListsController < ApplicationController
 
 	def update
     @list = List.find(params[:id])
-    # binding.pry
     if @list.update(list_params)
       redirect_to list_path(@list)
     else
